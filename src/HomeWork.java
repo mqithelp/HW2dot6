f1bd0f397b7c412caf6a68e7f409a259ad0abab4
MaxHome <mqithelp@gmail.com>
2024-09-01 01:23:09 +0300

    Небольшой код-рефакторинг.

---
+++ src/HomeWork.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class HomeWork {
+
+    public static void getOddNums(List<Integer> nums) {
+        List<Integer> odds = new ArrayList<>();
+        for (Integer num : nums) {
+            if (num % 2 != 0) odds.add(num);
+        }
+        System.out.println("Нечетные числа: " + odds);
+    }
+
+    public static void getEvenNums(List<Integer> nums) {
+        List<Integer> odds = new ArrayList<>();
+        for (Integer num : nums) {
+            if (num % 2 == 0) odds.add(num);
+        }
+        System.out.println("Четные числа: " + odds);
+    }
+
+    public static void printCountDuplicateWords(List<String> strings) {
+        Map<String, Integer> countList = new HashMap<>();
+        Integer count = 0;
+        for (String string : strings) {
+            countList.put(string, count);
+        }
+        for (String string : strings) {
+            if (countList.containsKey(string)) {
+                count = countList.get(string);
+                countList.put(string, count + 1);
+            }
+        }
+
+        System.out.println("Подсчет одинаковых слов: " + countList);
+    }
+
+    public static void printUniqueWords(List<String> strings) {
+        Set<String> uniqueWords = new HashSet<>(strings);
+        System.out.println("Уникальные слова: " + uniqueWords);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,17 @@
-import javax.sound.midi.Soundbank;
-import javax.swing.*;
-import java.sql.SQLOutput;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
 
-        getOddNums(nums);
-        getEvenNums(nums);
-        printUniqueWords(strings);
-        printCountDuplicateWords(strings);
-
+        HomeWork.getOddNums(nums);
+        HomeWork.getEvenNums(nums);
+        HomeWork.printUniqueWords(strings);
+        HomeWork.printCountDuplicateWords(strings);
 
     }
 
-    private static void getOddNums(List<Integer> nums) {
-        List<Integer> odds = new ArrayList<>();
-        for (Integer num : nums) {
-            if (num % 2 != 0) odds.add(num);
-        }
-        System.out.println("Нечетные числа: " + odds);
-    }
-
-    private static void getEvenNums(List<Integer> nums) {
-        List<Integer> odds = new ArrayList<>();
-        for (Integer num : nums) {
-            if (num % 2 == 0) odds.add(num);
-        }
-        System.out.println("Четные числа: " + odds);
-    }
-
-    private static void printCountDuplicateWords(List<String> strings) {
-        Map<String, Integer> countList = new HashMap<>();
-        Integer count = 0;
-        for (String string : strings) {
-            countList.put(string, count);
-        }
-        for (String string : strings) {
-            if (countList.containsKey(string)) {
-                count = countList.get(string);
-                countList.put(string, count + 1);
-            }
-        }
-
-        System.out.println("Подсчет одинаковых слов: " + countList);
-    }
-
-    private static void printUniqueWords(List<String> strings) {
-        Set uniqueWords = new HashSet<>(strings);
-        System.out.println("Уникальные слова: " + uniqueWords);
-    }
 
 }
