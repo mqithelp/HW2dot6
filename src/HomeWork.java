d51c5f1c4af6214b41914d0f9b79d1690fb1c0e1
MaxHome <mqithelp@gmail.com>
2024-09-02 01:41:42 +0300

    добавил метод

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -10,17 +10,31 @@ public class HomeWork {
         System.out.println("Нечетные числа: " + odds);
     }
 
-    public static void getEvenNums(List<Integer> nums) {
+    public static void getEvenNumsOne(List<Integer> nums) {
         Set<Integer> uniqueEven = new HashSet<>();
         Integer[] result;
+
         for (Integer num : nums) {
             if (num % 2 == 0) uniqueEven.add(num);
         }
-
         result = uniqueEven.toArray(new Integer[0]);
         Arrays.sort(result);
         System.out.println("Четные числа: " + Arrays.toString(result));
     }
+
+    public static void getEvenNumsTwo(List<Integer> nums) {
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        System.out.print("Четные числа: ");
+        for (Integer num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.print(num + " ");
+                prevNum = num;
+            }
+        }
+        System.out.println("");
+    }
+
 
     public static void printCountDuplicateWords(List<String> strings) {
         Map<String, Integer> countList = new HashMap<>();
